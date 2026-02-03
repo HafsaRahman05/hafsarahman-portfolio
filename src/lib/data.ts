7cc667e80770f7379bdbaeb03da924a88f09310c
Hafsa Rahman <hafsarahman805@gmail.com>
2026-02-03 17:45:22 +0000

    https://github.com/HafsaRahman05/image/blob/main/image%20copy%202.png (_

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -54,7 +54,7 @@ export const projects: Project[] = [
     liveLink: 'https://shifa-840.vercel.app/',
     githubLink: 'https://github.com/HafsaRahman05/SHIFA',
     category: 'Web',
-    image: '/shifa-dashboard.png',
+    image: 'https://raw.githubusercontent.com/HafsaRahman05/image/main/image%20copy%202.png',
     imageHint: 'hospital dashboard',
   },
   {
