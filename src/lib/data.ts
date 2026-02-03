8618fd4d8bd211b06c8e37d7a78b3be51bbb180e
Hafsa Rahman <hafsarahman805@gmail.com>
2026-02-03 17:55:35 +0000

    https://github.com/HafsaRahman05/image/blob/main/image%20copy.png
    use t

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -54,7 +54,7 @@ export const projects: Project[] = [
     liveLink: 'https://shifa-840.vercel.app/',
     githubLink: 'https://github.com/HafsaRahman05/SHIFA',
     category: 'Web',
-    image: 'https://raw.githubusercontent.com/HafsaRahman05/image/main/image%20copy%202.png',
+    image: 'https://raw.githubusercontent.com/HafsaRahman05/image/main/image%20copy.png',
     imageHint: 'hospital dashboard',
   },
   {
